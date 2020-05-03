53da0ea3855afb760ba316b3aaa5519ae2d6c84d
alazarte <alazarte@tutanota.com>
2020-05-03 11:45:39 -0300

    Update suckless

--- suckless/suckless/dwm/config.h
+++ suckless/suckless/dwm/config.h
@@ -15,7 +15,7 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_green[]        = "#00ff00";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_black, col_green },
+	[SchemeNorm] = { col_gray3, col_black, col_gray1 },
 	[SchemeSel]  = { col_green, col_black,  col_green  },
 };
 
@@ -59,11 +59,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_black, "-sf", col_green, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *lockcmd[]  = { "slock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_x,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
